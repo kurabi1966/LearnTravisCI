9fc50d8ebcebf95cb97d2adb45c783f0e9440f51
Ammar Kurabi <kurabi@gmail.com>
2018-11-12 15:56:05 +0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React by Examples.
           </a>
         </header>
       </div>
